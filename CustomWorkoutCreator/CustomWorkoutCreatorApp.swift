9beb4d4231995798c7e522f30b5aad7330f01da5
mkalkanderi <mohammedalkandari8@gmail.com>
2025-07-16 11:51:42 +0300

    Initial Commit

---
+++ CustomWorkoutCreator/CustomWorkoutCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomWorkoutCreatorApp.swift
+//  CustomWorkoutCreator
+//
+//  Created by Developer on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomWorkoutCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
